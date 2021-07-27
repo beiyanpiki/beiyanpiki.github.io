8c856b1cca8524881b9a43c8136c200d315f8bc5
beiyanpiki <sizegaoxin@126.com>
2021-07-28 01:58:43 +0800

    feat: update cross.html

--- src/index.js
+++ src/index.js
@@ -1,23 +1,24 @@
 function GetRequest() {
-    const url = location.search;
-    let theRequest = new Object();
-    if (url.indexOf("?") != -1) {
-        let str = url.substr(1);
-        strs = str.split("&");
-        for (let i = 0; i < strs.length; i++) {
-            theRequest[strs[i].split("=")[0]] = unescape(strs[i].split("=")[1]);
-        }
+  const url = location.search;
+  let theRequest = new Object();
+  if (url.indexOf("?") != -1) {
+    let str = url.substr(1);
+    strs = str.split("&");
+    for (let i = 0; i < strs.length; i++) {
+      theRequest[strs[i].split("=")[0]] = unescape(strs[i].split("=")[1]);
     }
-    return theRequest;
+  }
+  return theRequest;
 }
 
 window.onload = function () {
-    let Request = new Object();
-    Request = GetRequest();
-    no_background = Request['with_background']
-    if (no_background === 'true') {
-        document.getElementById("container").id = 'without-container';
-        document.getElementById("header-with-background").id = 'header-without-background';
-        document.getElementById("with-bg").id = 'no-bg';
-    }
-}
+  let Request = new Object();
+  Request = GetRequest();
+  with_background = Request["bg"];
+  if (with_background === "true") {
+    document.getElementById("no-container").id = "container";
+    document.getElementById("header-without-background").id =
+      "header-with-background";
+    document.getElementById("no-bg").id = "bg";
+  }
+};
