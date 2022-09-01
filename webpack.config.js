1c26a9f98576c8456d71df1f412210e0cac0d0aa
Shane Miku <sizegaoxin@126.com>
2022-09-01 22:54:14 +0800

    update some information

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
+
 module.exports = {
     context: path.resolve(__dirname, 'src'),
 
@@ -60,6 +61,6 @@ module.exports = {
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
             template: 'index.html'
-        })
+        }),
     ]
 }
